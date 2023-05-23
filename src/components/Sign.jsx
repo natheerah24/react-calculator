322d204d34c12318f2e2d4e8370d5e6f07835e7b
natheerah24 <natheerah24@gmail.com>
2023-05-23 16:04:31 +0200

    Tailwind styling and calculator provider.

--- src/components/Sign.jsx
+++ src/components/Sign.jsx
@@ -1,7 +1,9 @@
 import propTypes from 'prop-types';
+
 const Sign = (props) => {
     return (
-        <sign className={"bg-amber-500 font-bold text-white rounded-full px-10 py-5 hover:bg-white hover:text-amber-500"}>{props.signLabel}</sign>
+        <sign
+            className={"bg-amber-500 font-bold text-white rounded-full px-10 py-5 hover:bg-white hover:text-amber-500"}>{props.signLabel}</sign>
     )
 }
 Sign.propTypes = {
