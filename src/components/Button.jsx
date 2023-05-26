8cc34aceafac443bcc21a593bfb9e9937ff93a70
natheerah24 <natheerah24@gmail.com>
2023-05-26 14:17:43 +0200

    Added calculation functionality

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,20 +1,46 @@
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import { useCalculatorDispatch } from "../contexts/Calculator.jsx";
 
 const buttonStyles = {
     normal: 'bg-slate-500 rounded-md hover:bg-slate-600 ease-in duration-300',
     zinc: 'bg-zinc-800 hover:bg-zinc-700',
     amber: 'bg-amber-500 hover:bg-white hover:text-amber-500',
     gray: 'bg-gray-500 hover:bg-gray-400'
-}
-export function Button (props) {
+};
+
+export function Button(props) {
+    const calculatorDispatch = useCalculatorDispatch();
+
+    const handleButton = (input) => {
+        if (input === 'AC') {
+            calculatorDispatch({ type: 'clear' });
+            return;
+        }
+        if (input === '=') {
+            calculatorDispatch({ type: 'evaluate' });
+            return;
+        }
+        if (input === '×') {
+            input = '*';
+        }
+        if (input === '÷') {
+            input = '/';
+        }
+        calculatorDispatch({ type: 'input', input });
+    };
+
     return (
         <button
-            className={`rounded-full px-10 py-5 font-bold text-white text-center ${buttonStyles[props.style || 'zinc']} ${props.className}`}>{props.buttonLabel}
+            onClick={() => handleButton(props.buttonLabel)}
+            className={`rounded-full px-10 py-5 font-bold text-white text-center ${buttonStyles[props.style || 'zinc']} ${props.className}`}
+        >
+            {props.buttonLabel}
         </button>
-    )
+    );
 }
+
 Button.propTypes = {
-    buttonLabel: propTypes.string,
-    style: propTypes.string,
-    className: propTypes.string,
-}
+    buttonLabel: PropTypes.string,
+    style: PropTypes.string,
+    className: PropTypes.string,
+};
