b45731e8c36ef61556b05f0fdf820c8bbc0338a0
natheerah24 <natheerah24@gmail.com>
2023-05-30 15:38:28 +0200

    overflow hide

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -3,7 +3,7 @@ import {useCalculator} from "../contexts/Calculator.jsx";
 const Display = () => {
     const calculator = useCalculator();
     return (
-        <div className={"bg-black rounded-lg m-5 text-white text-right p-7 border-2 border-white font-display"}>
+        <div className={"bg-black rounded-lg m-5 text-white text-right p-7 border-2 border-white font-display overflow-auto"}>
             <span
                 className={`font-mono text-4xl ${!calculator.evaluationString && !calculator.result ? 'animate-blink' : ''}`}>{calculator.evaluationString || calculator.result}</span>
         </div>
